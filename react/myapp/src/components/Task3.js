a862c700d70a43ac13b73fcc8957d405195f55de
sarang04 <59948900+sarang04@users.noreply.github.com>
2020-04-10 09:48:12 +0530

    Add files via upload
    Task3

---
+++ react/myapp/src/components/Task3.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+
+
+class Task3 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+  
+    // change code below this line
+  
+    increment() {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
+    
+    decrement() {
+      this.setState({
+        count: this.state.count - 1
+      });
+    };
+  
+    reset() {
+      this.setState({
+        count: 0
+      });
+    };
+  
+    // change code above this line
+    render() {
+      return (
+  
+     <div>
+     <button className='inc' onClick={(e) => this.increment(e)}>Increment!</button>
+      <button className='dec' onClick={(e) => this.decrement(e)}>Decrement!</button>
+      <button className='reset' onClick={(e) => this.reset(e)}>Reset</button>
+      <h1>Current Count: {this.state.count}</h1>
+    </div>
+      );
+    }
+  };
+  export default Task3
