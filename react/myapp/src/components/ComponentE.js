b90007ab9bb76c4154809e78bb5bd6a18248b53a
sarang04 <59948900+sarang04@users.noreply.github.com>
2020-04-10 18:22:44 +0530

    Task4

---
+++ react/myapp/src/components/ComponentE.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ComponentF from './ComponentF'
+import UserContext from './userContext'
+ class ComponentE extends Component {
+    render() {
+        return (
+<div>
+    Component E context {this.context}
+    <ComponentF />
+
+</div>
+            )
+    }
+}
+ComponentE.contextType = UserContext
+export default ComponentE
